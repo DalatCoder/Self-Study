eedb9d7d9d34bb2fd70be088a88a1b1828f5a1a7
DalatCoder <hieuntctk42@gmail.com>
2019-04-01 22:00:48 +0700

    Basic JavaScript: Updating Object Properties

--- FreeCodeCamp/Javascript/object.js
+++ FreeCodeCamp/Javascript/object.js
@@ -28,4 +28,24 @@ var testObj = {
 var playerNumber = 16;       // Change this Line
 var player = testObj[playerNumber];   // Change this Line
 
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.name = "Happy Coder";
 
