366ae8a1168fb4284efcaeda3ebf996132939ef6
DalatCoder <hieuntctk42@gmail.com>
2019-04-02 11:20:39 +0700

    ES6: Use Destructuring Assignment with the Rest Operator to Reassign Array Elements

---
+++ FreeCodeCamp/Javascript/ES6/destructingArray.js
@@ -0,0 +1,26 @@
+/**
+ * Swap two values using detructing array
+ */
+
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  [a, b] = [b, a];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  const [a, b, ...arr] = list; // change this
+  // change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
+
