d3a03d8406e14af3d3f28bc69745d1fb0f13c01d
DalatCoder <hieuntctk42@gmail.com>
2019-04-02 08:55:45 +0700

    Finish Basic javascript

---
+++ FreeCodeCamp/Javascript/ES6/constIsMutable.js
@@ -0,0 +1,35 @@
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  // change code below this line
+
+  // s = [2, 5, 7]; <- this is invalid
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+  // change code above this line
+}
+editInPlace();
+
+// Prevent object mutation
+// Use Object.freeze() method
+function freezeObj() {
+  "use strict";
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // change code below this line
+
+  Object.freeze(freezeObj);
+  // change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch( ex ) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+
+
